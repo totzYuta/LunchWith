78e9408aa14372d22096870aff74abcf0532889a
Keita Nakanishi <contact@keita.co>
2014-09-12 17:28:39 +0900

    データベースの接続部分を作成

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+require_once(dirname(__FILE__)."/db.php");
+
+try {
+    $dbh = new PDO(DSN, DB_USER, DB_PASSWORD);
+} catch (PDOException $e) {
+    var_dump($e->getMessage());
+    exit;
+}
+
+// $sql = "select * from sites";
+// $stmt = $dbh->query($sql);
+
+// $sql2 = "insert ignore into articles (site_id, title, url, date, img_path, description) values (?,?,?,?,?,?)";
+// $stmt2 = $dbh->prepare($sql2);
+// $stmt2->execute(array($site_id, $title, $url, $date, $img_path, $striped));
+
+// $dbh = null;
