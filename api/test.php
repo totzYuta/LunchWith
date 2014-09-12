d08e25fa42943fd6ca3da11393c4a7cf65a9f0fd
Keita Nakanishi <contact@keita.co>
2014-09-12 17:39:05 +0900

    jsonを返すテストのapi作成

---
+++ api/test.php
@@ -0,0 +1,9 @@
+<?php
+require_once(dirname(__FILE__)."/../connect.php");
+
+$sql = "select * from user";
+$stmt = $dbh->query($sql);
+
+echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+
+$dbh = null;
