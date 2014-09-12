a5a449e0ade6b92a63742b2ccc206967323721ca
totzYuta <zfbfmtmxaz@gmail.com>
2014-09-12 22:13:35 +0900

    api/offerers.phpつくれた

--- api/offerers.php
+++ api/offerers.php
@@ -1,8 +1,23 @@
 <?php
 require_once(dirname(__FILE__)."/../connect.php");
 
-$sql = "SELECT * FROM offers WHERE active=1 AND id=".$_GET['id'];
-echo $sql;
+$sql_offerers = "SELECT * FROM offers WHERE id=".$_GET['id'];
+
+$offerer_id_array = array();
+$row = $dbh->query($sql_offerers);
+/*while($row) {
+  array_push($offerer_id_array, $row[0]);
+}*/
+/*for ($i=0; count($row)<$i; $i++) {
+  array_push($offerer_id_array, $row[0]);
+}*/
+
+$tmp_sql = implode(" OR id=", $offerer_id_array);
+
+$sql = "SELECT * FROM users WHERE id=".$tmp_sql;
+
+echo $sql."<br>";
+
 $stmt = $dbh->query($sql);
 
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
