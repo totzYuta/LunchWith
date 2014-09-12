f2a6fdebbe9d2f55dcc70b3b1e4f422dd7de1811
totzYuta <zfbfmtmxaz@gmail.com>
2014-09-12 22:16:28 +0900

    api/friends.phpを修正

--- api/friends.php
+++ api/friends.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(__FILE__)."/../connect.php");
 
-$sql = "select * from users where id=" . $_GET['id'];
+$sql = "select * from users where not id=" . $_GET['id'];
 $stmt = $dbh->query($sql);
 
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
