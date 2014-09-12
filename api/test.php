77e06cc4e10d563fc2c31ef038200610474392dc
Keita Nakanishi <contact@keita.co>
2014-09-12 23:01:54 +0900

    テーブル名の修正

--- api/test.php
+++ api/test.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(__FILE__)."/../connect.php");
 
-$sql = "select * from user";
+$sql = "select * from users";
 $stmt = $dbh->query($sql);
 
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
